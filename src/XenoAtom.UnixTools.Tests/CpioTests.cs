4dac39a011ecd70f0a8ce2eb96d8ddbbad6f83b8
Alexandre Mutel <alexandre_mutel@live.com>
2024-09-08 13:49:50 +0200

    Improve CpioTests

--- src/XenoAtom.UnixTools.Tests/CpioTests.cs
+++ src/XenoAtom.UnixTools.Tests/CpioTests.cs
@@ -50,12 +50,12 @@ public class CpioTests
         var fileStreamOut = new MemoryStream();
 
         var fs = new UnixMemoryFileSystem();
+        var entries = new List<CpioEntry>();
 
         // Use a block to dispose the reader/writer
         {
             using var reader = new CpioReader(fileStream, true);
             using var writer = new CpioWriter(fileStreamOut, true);
-            var entries = new List<CpioEntry>();
             while (reader.TryGetNextEntry(out var entry))
             {
                 entries.Add(entry);
@@ -76,23 +76,76 @@ public class CpioTests
         {
             using var reader = new CpioReader(fileStream, true);
             using var writer = new CpioWriter(fileStreamOut, true);
-            Console.WriteLine("Reading from cpio");
             fs.ReadFrom(reader);
-            Console.WriteLine("Writing to cpio");
             fs.WriteTo(writer);
         }
         
         var bufferIn = fileStream.ToArray();
         var bufferOut = fileStreamOut.ToArray();
 
-        File.WriteAllBytes("cpio1.cpio", bufferIn);
-        File.WriteAllBytes("cpio2.cpio", bufferOut);
+        //File.WriteAllBytes("cpio1.cpio", bufferIn);
+        //File.WriteAllBytes("cpio2.cpio", bufferOut);
 
         // We check that we have the same content
         Assert.AreEqual(bufferIn.Length, bufferOut.Length, "Invalid length of the generated archive");
         for (int i = 0; i < bufferIn.Length; i++)
         {
             Assert.AreEqual(bufferIn[i], bufferOut[i], $"Invalid value 0x{bufferOut[i]:X2} at position {i} / 0x{i:X4}");
+        }
+
+        // Check that we have the same entries
+        var entriesOut = new List<CpioEntry>();
+        fileStreamOut.Position = 0;
+        using (var reader = new CpioReader(fileStreamOut, true))
+        {
+            while (reader.TryGetNextEntry(out var entry))
+            {
+                entriesOut.Add(entry);
+            }
+        }
+
+        Assert.AreEqual(entries.Count, entriesOut.Count, "Invalid number of entries");
+        for (int i = 0; i < entries.Count; i++)
+        {
+            var entry = entries[i];
+            var entryOut = entriesOut[i];
+
+            Assert.AreEqual(entry.Name, entryOut.Name, "Invalid name");
+            Assert.AreEqual(entry.InodeNumber, entryOut.InodeNumber, "Invalid inode number");
+            Assert.AreEqual(entry.FileType, entryOut.FileType, "Invalid file type");
+            Assert.AreEqual(entry.Dev, entryOut.Dev, "Invalid dev");
+            Assert.AreEqual(entry.Mode, entryOut.Mode, "Invalid mode");
+            Assert.AreEqual(entry.Uid, entryOut.Uid, "Invalid uid");
+            Assert.AreEqual(entry.Gid, entryOut.Gid, "Invalid gid");
+            Assert.AreEqual(entry.Length, entryOut.Length, "Invalid length");
+            Assert.AreEqual(entry.HardLinkCount, entryOut.HardLinkCount, "Invalid hard link count");
+            Assert.AreEqual(entry.Device, entryOut.Device, "Invalid device");
+            Assert.AreEqual(entry.ModificationTime, entryOut.ModificationTime, "Invalid modification time");
+
+            Assert.AreEqual(entry.LinkName, entryOut.LinkName, "Invalid link name");
+
+            if (entry.DataStream == null)
+            {
+                Assert.IsNull(entryOut.DataStream, "Invalid content");
+            }
+            else
+            {
+                Assert.IsNotNull(entryOut.DataStream, "Invalid content");
+                Assert.AreEqual(entry.DataStream.Length, entryOut.DataStream.Length, "Invalid content length");
+
+                var contentIn = new MemoryStream();
+                var contentOut = new MemoryStream();
+                entry.DataStream.Position = 0;
+                entry.DataStream.CopyTo(contentIn);
+                entryOut.DataStream.Position = 0;
+                entryOut.DataStream.CopyTo(contentOut);
+
+                Assert.AreEqual(contentIn.Length, contentOut.Length, "Invalid content length");
+                for (int j = 0; j < contentIn.Length; j++)
+                {
+                    Assert.AreEqual(contentIn.GetBuffer()[j], contentOut.GetBuffer()[j], $"Invalid content at position {j}");
+                }
+            }
         }
     }
 }
